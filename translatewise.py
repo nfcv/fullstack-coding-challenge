0f0b574f9a68ef601666972ee015a1e5065050fb
NFCV <nx.entertainment.pt@gmail.com>
2018-09-26 22:15:53 +0100

    translate wise application front-end skeleton

--- translatewise.py
+++ translatewise.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+translations = [
+    {
+        'en_UK': 'Hello',
+        'es_ES': 'Holla',
+        'status': 'requested'
+    },
+    {
+        'en_UK': 'How you doing?',
+        'es_ES': 'Como estas?',
+        'status': 'pending'
+    },
+    {
+        'en_UK': 'How',
+        'es_ES': 'Como',
+        'status': 'translated'
+    }
+]
 
 @app.route("/")
 def index():
-    return "<h1>Translate Wise</h1>"
-
+    return render_template("index.html", translations=translations)
 
 if __name__ == '__main__':
     app.run(debug=True)
