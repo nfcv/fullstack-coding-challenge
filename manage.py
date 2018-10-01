867aefc6b877cf566fea8cb68291da41f36ba242
NFCV <nx.entertainment.pt@gmail.com>
2018-10-01 02:32:34 +0100

    migrate commands

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 import os
-from translatewise import create_app
+from translatewise import create_app, db
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 APP_CONFIG = os.environ.get("APP_CONFIG")
 
 app = create_app()
-
+migrate = Migrate(app, db)
 manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
