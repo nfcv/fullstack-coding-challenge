04ba2860463c0431060258fa3db588affd647abc
NFCV <nx.entertainment.pt@gmail.com>
2018-10-01 02:13:30 +0100

    Load translations from database

--- translatewise/translations/routes.py
+++ translatewise/translations/routes.py
@@ -1,36 +1,21 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from translatewise import db
 from translatewise.translations.forms import TranslationForm
 from translatewise.translations.models import Translation
 
-translations = Blueprint('translations', __name__)
 
-data = [
-    {
-        'en_UK': 'Hello',
-        'es_ES': 'Holla',
-        'status': 'requested'
-    },
-    {
-        'en_UK': 'How you doing?',
-        'es_ES': 'Como estas?',
-        'status': 'pending'
-    },
-    {
-        'en_UK': 'How',
-        'es_ES': 'Como',
-        'status': 'translated'
-    }
-]
+translations = Blueprint('translations', __name__)
 
 
 @translations.route("/", methods=['GET', 'POST'])
 def index():
+    data = db.session.query(Translation).all()
+
     form = TranslationForm()
     if form.validate_on_submit():
         translation = Translation(form.text.data)
         db.session.add(translation)
         db.session.commit()
         flash(f'Translation added!', 'success')
-        
+        return redirect(url_for("translations.index"))
     return render_template("index.html", translations=data, form=form)
