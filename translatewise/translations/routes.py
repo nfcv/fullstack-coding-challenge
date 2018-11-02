558155607bce7cefe37ae392e3b5d9cd77949a3d
NFCV <nx.entertainment.pt@gmail.com>
2018-11-02 03:01:03 +0000

    added a interactor factory to simplify object creation

--- translatewise/translations/routes.py
+++ translatewise/translations/routes.py
@@ -1,9 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, request, current_app
+from flask import Blueprint, render_template, redirect, url_for, request
 from translatewise.translations.forms import TranslationForm
-from translatewise.translations.interactors.translation_interactor import TranslationInteractor
-from translatewise.translations.services.translation_service import TranslationService
-from translatewise.translations.services.translation_queue_service import TranslationQueueService
-from translatewise.translations.repositories.translation_repo import TranslationRepo
+from translatewise.translations.interactors.factory import TranslationInteractorFactory
 
 
 translations = Blueprint('translations', __name__)
@@ -11,15 +8,7 @@ translations = Blueprint('translations', __name__)
 
 @translations.route("/", methods=['GET', 'POST'])
 def home():
-    repo = TranslationRepo()
-    queue = current_app.task_queue
-    queue_service = TranslationQueueService(queue=queue)
-    translation_service = TranslationService(repository=repo)
-
-    interactor = TranslationInteractor(
-        translation_service=translation_service,
-        queue_service=queue_service
-    )
+    interactor = TranslationInteractorFactory.create()
 
     form = TranslationForm(request.form)
     if request.method == "POST":
